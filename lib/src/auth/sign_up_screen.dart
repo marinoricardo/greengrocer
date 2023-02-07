4a9fbe420ce61ccaec6d26f14779733464edb18a
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2023-02-07 16:53:10 +0200

    signupscreen done

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/components/custom_text_field.dart';
@@ -9,42 +9,69 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: CustomColors.customSwatchColor,
-      body: Column(
-        children: [
-          Expanded(
-            child: Center(
-              child: Text(
-                'Cadastro',
-                style: TextStyle(fontSize: 35, color: Colors.white),
+      appBar: AppBar(
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          child: Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text(
+                    'Cadastro',
+                    style: TextStyle(fontSize: 35, color: Colors.white),
+                  ),
+                ),
               ),
-            ),
-          ),
 
-          // formulario de registo
-          Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 32,
-              vertical: 40,
-            ),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(
-                top: Radius.circular(45),
-              ),
-            ),
-            child: Column(
-              children: [
-                CustomTextField(label: 'E-mail', icon: Icons.email),
-                CustomTextField(label: 'Senha', icon: Icons.lock),
-                CustomTextField(label: 'Nome', icon: Icons.person),
-                CustomTextField(label: 'Celular', icon: Icons.phone),
-                CustomTextField(label: 'CPF', icon: Icons.file_copy),
-              ],
-            ),
-          )
-        ],
+              // formulario de registo
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 40,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(45),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    CustomTextField(label: 'E-mail', icon: Icons.email),
+                    CustomTextField(label: 'Senha', icon: Icons.lock),
+                    CustomTextField(label: 'Nome', icon: Icons.person),
+                    CustomTextField(label: 'Celular', icon: Icons.phone),
+                    CustomTextField(label: 'CPF', icon: Icons.file_copy),
+                    SizedBox(
+                      height: 50,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Cadastrar usuario',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
