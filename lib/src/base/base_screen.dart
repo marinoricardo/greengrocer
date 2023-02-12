9243838849e875f5cdf6b566e4eb60a42133529e
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2023-02-12 12:30:05 +0200

    bottomNAvigationBar done

--- lib/src/base/base_screen.dart
+++ lib/src/base/base_screen.dart
@@ -1,10 +1,61 @@
+// ignore_for_file: prefer_final_fields, unused_field, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-class BaseScreen extends StatelessWidget {
+class BaseScreen extends StatefulWidget {
   const BaseScreen({Key? key}) : super(key: key);
 
   @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  PageController _pageController = PageController();
+  int currentIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        controller: _pageController,
+        children: [
+          Container(
+            color: Colors.amber,
+          ),
+          Container(
+            color: Colors.purple,
+          ),
+          Container(
+            color: Colors.red,
+          ),
+          Container(
+            color: Colors.black,
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (page) {
+          setState(() {
+            currentIndex = page;
+          });
+          _pageController.animateToPage(page,
+              duration: Duration(milliseconds: 3), curve: Curves.ease);
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.green,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white.withAlpha(100),
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart_outlined), label: 'Carinho'),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Produtos'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline), label: 'Perfil'),
+        ],
+      ),
+    );
   }
 }
