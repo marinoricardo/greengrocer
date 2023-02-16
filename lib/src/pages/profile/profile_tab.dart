fe9142a2bc441abe3811be9df4a7c3ee99ee10f6
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2023-02-16 07:50:27 +0200

    profile forms

--- lib/src/pages/profile/profile_tab.dart
+++ lib/src/pages/profile/profile_tab.dart
@@ -1,10 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:greengrocer/src/pages/components/custom_text_field.dart';
 
 class ProfileTab extends StatelessWidget {
   const ProfileTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Perfil'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.logout,
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.fromLTRB(16, 32, 16, 16),
+        children: const [
+          CustomTextField(
+            label: 'E-mail',
+            icon: Icons.email,
+          ),
+          CustomTextField(
+            label: 'Nome',
+            icon: Icons.person,
+          ),
+          CustomTextField(
+            label: 'Celular',
+            icon: Icons.phone,
+          ),
+        ],
+      ),
+    );
   }
 }
